8d4fd34b6bb9c1556ce232809afd91fed4388a56
Jon Cursi <joncursi@gmail.com>
2024-09-25 19:35:59 -0400

    feat: Upgraded prettier to 3.3.3, as well as associated eslint packages

--- examples/ReactComponent/index.test.tsx
+++ examples/ReactComponent/index.test.tsx
@@ -4,8 +4,8 @@ import ReactComponent from '.';
 
 test('renders button and handles click events', () => {
   const { getByText } = render(<ReactComponent />);
-
   const button = getByText(/Button Clicks:/i);
+
   expect(button).toBeInTheDocument();
   expect(button).toHaveTextContent('Button Clicks: 0');
 
